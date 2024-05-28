e9705fd2d88d184ceba26d7eb47e2b43e91ec990
tanmayeekyram <tanmayeekyram@gmail.com>
2024-05-28 21:14:28 +0530

    THIS IS THE LAST ONE

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -34,7 +34,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
         buyerId: order.buyerId,
       },
       mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
 
