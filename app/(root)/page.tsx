851cb70d5ed9f3c087558cc1915d91f4c7182bbb
tanmayeekyram <tanmayeekyram@gmail.com>
2024-06-07 20:54:34 +0530

    changes in the logos and symbols

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
-import CategoryFilter from "@/components/shared/CategoryFilter";
-import Collection from "@/components/shared/Collection";
-import Search from "@/components/shared/Search";
-import { Button } from "@/components/ui/button";
-import { getAllEvents } from "@/lib/actions/event.actions";
-import { SearchParamProps } from "@/types";
-import Image from "next/image";
-import Link from "next/link";
-
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
@@ -20,20 +19,20 @@ export default async function Home({ searchParams }: SearchParamProps) {
     limit: 6
   })
 
-
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold"> Host, Connect, Participate: One platform for all VNRVJIET events!</h1>
-            <p className="p-regular-20 md:p-regular-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti officiis distinctio molestias dolore quaerat?</p>
+            <h1 className="h1-bold">Host, Connect, Celebrate: One platform for all VNRVJIET events!</h1>
+            <p className="p-regular-20 md:p-regular-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatem inventore sit laboriosam.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
-                Explore more
+                Explore Now
               </Link>
             </Button>
           </div>
+
           <Image 
             src="/assets/images/hero.png"
             alt="hero"
@@ -42,16 +41,17 @@ export default async function Home({ searchParams }: SearchParamProps) {
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
-      </section>
+      </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-      <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
-      
-      <div className="flex w-full flex-col gap-5 md:flex-row">
-        <Search />
-        <CategoryFilter />
-      </div>
-      <Collection 
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+
+        <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -62,5 +62,5 @@ export default async function Home({ searchParams }: SearchParamProps) {
         />
       </section>
     </>
-  );
+  )
 }
